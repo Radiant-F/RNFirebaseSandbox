27d2c621e4111b3edf6b152934e5be1c59f245f5
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-11-27 08:43:12 +0700

    initial commit

---
+++ src/features/chat/components/NotificationPermission.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {localStorage} from '../../../utils';
+
+export default function NotificationPermission() {
+  useEffect(() => {}, []);
+
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
