2c07d4390e38eca58f651081c8bdf8eedaa8a3b5
Radiant Fadilah <radiantfadilah0@gmail.com>
2024-12-11 14:54:32 +0700

    initial commit

---
+++ src/features/social/index.ts
@@ -0,0 +1,64 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../routes/type';
+
+export type SocialScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'Social'
+>;
+export type SocialPersonalScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'SocialPersonal'
+>;
+export type PostCreateScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'PostCreate'
+>;
+export type PostUpdateScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'PostUpdate'
+>;
+export type PostDetailScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'PostDetail'
+>;
+
+export type PostType = {
+  title: string;
+  description: string;
+  mediaUrl: {
+    url: string;
+    fileType: string;
+  }[];
+  createdAt: string;
+  updatedAt: string;
+  likes: {[userId: string]: boolean};
+  likeCount: number;
+  creator: UserType;
+  id: string;
+};
+export type MediaFileType = {
+  fileUri: string;
+  fileName: string;
+  fileType: string;
+};
+
+import {
+  useSocialPosts,
+  useCreatePost,
+  useUpdatePost,
+  useDeletePost,
+  useSocialPostsPersonal,
+} from './services/socialApi';
+export {
+  useSocialPosts,
+  useCreatePost,
+  useUpdatePost,
+  useDeletePost,
+  useSocialPostsPersonal,
+};
+
+import RenderPost from './components/RenderPost';
+import ProfileHeader from './components/ProfileHeader';
+import RenderPostDetail from './components/RenderPostDetail';
+import {UserType} from '../authentication';
+export {RenderPost, ProfileHeader, RenderPostDetail};
