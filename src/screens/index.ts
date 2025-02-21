bc79625213973c9338b79492d31511eae4021dee
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-02-21 14:37:12 +0700

    initial commit

---
+++ src/screens/index.ts
@@ -0,0 +1,37 @@
+// authentication
+import Authentication from './authentication';
+export {Authentication};
+
+// splash
+import Splash from './splash';
+export {Splash};
+
+// home
+import Home from './home';
+export {Home};
+
+// user profile
+import UserProfile from './user-profile';
+export {UserProfile};
+
+import Demo from './__test__';
+export {Demo};
+
+// task
+import Task from './task';
+export {Task};
+
+// social
+import Social from './social';
+import SocialPersonal from './social/SocialPersonal';
+import PostCreate from './social/PostCreate';
+import PostUpdate from './social/PostUpdate';
+import PostDetail from './social/PostDetail';
+export {Social, SocialPersonal, PostCreate, PostUpdate, PostDetail};
+
+// chat
+import Chat from './chat';
+import SearchUser from './chat/SearchUser';
+import ContactList from './chat/ContactList';
+import ChatScreen from './chat/ChatScreen';
+export {Chat, SearchUser, ContactList, ChatScreen};
